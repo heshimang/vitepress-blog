19cb7a856186a4457828e8e3482f74a43027fd0a
heshimang <hsm15031585502@163.com>
2022-03-11 16:04:54 +0800

    ci: 修改打包目录

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -4,6 +4,7 @@ import type { DefaultThemeOptions } from 'vuepress'
 export default defineUserConfig<DefaultThemeOptions>({
   // 站点配置
   lang: 'en-US',
+  base: '/vuepressv2-blog/',
   title: '前端小贺的博客',
   description: '前端小贺，力争做一个传道受业解惑的智者，专注于前端，但不限于前端，前端，贺士忙',
   head: [['link', { rel: 'icon', href: '/images/logo.png' }]],
