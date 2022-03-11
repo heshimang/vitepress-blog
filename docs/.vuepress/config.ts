1a2cb094770e5db8262661fd1361ce663294627a
heshimang <hsm15031585502@163.com>
2022-03-11 16:00:30 +0800

    refactor: 项目初始化

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,24 @@
+import { defineUserConfig } from 'vuepress'
+import type { DefaultThemeOptions } from 'vuepress'
+
+export default defineUserConfig<DefaultThemeOptions>({
+  // 站点配置
+  lang: 'en-US',
+  title: '前端小贺的博客',
+  description: '前端小贺，力争做一个传道受业解惑的智者，专注于前端，但不限于前端，前端，贺士忙',
+  head: [['link', { rel: 'icon', href: '/images/logo.png' }]],
+
+  // 主题和它的配置
+  theme: '@vuepress/theme-default',
+  themeConfig: {
+    logo: '/images/logo-small.png',
+    navbar: [
+      { text: '首页', link: '/'},
+      { text: 'JavaScript', link: '/javascript/'},
+      { text: 'Vue', link: '/vue/'},
+      { text: '前端架构与工程', link: '/framework/'},
+      { text: '生活所思', link: '/life/'},
+      { text: 'GitHub', link: 'https://github.com/heshimang'}
+    ]
+  },
+}) 
